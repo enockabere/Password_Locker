878b6f7b102345ea09ea3daaab9837fa06682fd6
enockabere <maebaenock95@gmail>
2021-07-30 17:49:14 +0300

    test to check if we can retrieve data from users list

--- user_test.py
+++ user_test.py
@@ -12,7 +12,7 @@ class testUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.myUser = User("Enock","Enock123") # create contact object
+        self.newUser = User("Enock","Enock123") # create contact object
     def tearDown(self):
         '''
         tearDown does cleanup after every test case has run
@@ -24,21 +24,21 @@ class testUser(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.myUser.username,"Enock")
-        self.assertEqual(self.myUser.password,"Enock123")
+        self.assertEqual(self.newUser.username,"Enock")
+        self.assertEqual(self.newUser.password,"Enock123")
         
     def test_save_user(self):
         '''
         test_save_user test case to test if user object is saved into the user list
         '''
-        self.myUser.save_user() # saving a new user
+        self.newUser.save_user() # saving a new user
         self.assertEqual(len(User.myUser),1)
         
     def test_save_multi_users(self):
         '''
         test_save_multi_user test case to test if we can save multiple objects to our contact lists
         '''
-        self.myUser.save_user()
+        self.newUser.save_user()
         test_user = User("Aiden","Aiden1234") #new user
         test_user.save_user()
         self.assertEqual(len(User.myUser),2)
@@ -46,12 +46,23 @@ class testUser(unittest.TestCase):
         '''
         test_delete_user test case to test if we can delete a user object from our user list
         '''
-        self.myUser.save_user()
+        self.newUser.save_user()
         test_user = User("Aiden","Aiden1234")
         test_user.save_user()
         
-        self.myUser.delete_user() #deleting a user
+        self.newUser.delete_user() #deleting a user
         self.assertEqual(len(User.myUser),1)
+    def test_find_user_by_username(self):
+        '''
+        test to check if we can retrieve a user by username and display information
+        '''
+        self.newUser.save_user()
+        test_user = User("Aiden","Aiden1234")
+        test_user.save_user()
+        
+        found_user = User.find_by_username("Aiden")
+        self.assertEqual(found_user.password,test_user.password)
+    
 
 if __name__ ==  '__main__':
     unittest.main()
