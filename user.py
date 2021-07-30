edd44ea8e108d3dc1c5f7c5284daaf30b0a4266e
enockabere <maebaenock95@gmail>
2021-07-30 14:21:29 +0300

    create a user class that generates new instances of user

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    """
+    Class that generates new instances of users.
+    """
+
+    myUser = [] # Empty contact list
+
+    def __init__(self,username,password):
+
+      # Create instances of objects
+
+        self.username = username
+        self.password = password
+    
